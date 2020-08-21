50df84f8bee706f48d63143523deb50a14177cde
Michael Oldacre <mloldacre@live.com>
2020-08-21 18:41:00 -0400

    Setting up skeleton

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  //smoke test
+  it('renders App component without errors', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
 });
