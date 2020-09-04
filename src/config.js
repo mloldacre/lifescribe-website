0c331bc5283c5172b6834c8a864222821d502ba0
Michael Oldacre <mloldacre@live.com>
2020-09-04 11:34:54 -0400

    configuring api connection

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  API_ENDPOINT: 'http://localhost:8000/api',
+  TOKEN_KEY: '014420fe-67a4-4c64-baf0-5d49481ab411',
+}
---
+++ src/services/scribe-api-service.js
@@ -0,0 +1,59 @@
+import TokenService from '../services/token-service'
+import config from '../config'
+
+const ScribeApiService = {
+  getScribes() {
+    return fetch(`${config.API_ENDPOINT}/Scribes`, {
+      headers: {
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  getScribe(scribeId) {
+    return fetch(`${config.API_ENDPOINT}/Scribes/${scribeId}`, {
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  getScribeScribbles(scribeId) {
+    return fetch(`${config.API_ENDPOINT}/scribes/${scribeId}/scribbles`, {
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  postScribe(userId) {
+    return fetch(`${config.API_ENDPOINT}/scribes`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        user_id: userId
+      }),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  }
+}
+
+export default ScribeApiService
