672165a7e74e6acf53d912a58d98811d6f04d40f
Michael Oldacre <mloldacre@live.com>
2020-09-20 21:52:34 -0400

    More styling

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -9,6 +9,7 @@ export default function Landing() {
       <Link to='/loginRegister'>
         <h1>Subscribe to YOUR life!</h1>
       </Link>
+      <h4>LifeScribe is a time-based web journal application that allows you to review your entries based on time and date.</h4>
       <Link to='/demo'>
         Try a Scribe!
       </Link>
