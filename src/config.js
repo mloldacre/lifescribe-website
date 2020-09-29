406b2accae88e42f57ac2422a687e52887f988d9
Michael Oldacre <mloldacre@live.com>
2020-09-29 14:16:39 -0400

    Re-deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-	API_ENDPOINT: `http://localhost:8000/api`,
+	API_ENDPOINT: `https://lit-bastion-34320.herokuapp.com/api`,
 	TOKEN_KEY: process.env.REACT_APP_API_KEY,
 };
 
-// https://lit-bastion-34320.herokuapp.com/api
+// http://localhost:8000/api
